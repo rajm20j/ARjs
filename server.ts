7627c8d63e6a2fa07e7e90bd250817f57d91d6cb
rajm20j <rajm20j@gmail.com>
2021-12-19 03:18:45 +0530

    Changes for heroku

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ const app = express();
 import path = require('path');
 const port: string | number = process.env.PORT || 8080;
 
-app.set('views', (path.join(__dirname, 'src\\static')));
+app.set('views', (path.join(__dirname, 'src/static')));
 app.set('view engine', 'ejs');
 
 app.get('/', (req: any, res: any) => {
